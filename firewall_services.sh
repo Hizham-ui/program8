8e39a57f173dbf2bd9107a5e60b85b487797a6ee
Hizham-ui <hizhamxo@gmail.com>
2026-09-11 11:31:03 +0530

    Update firewall_services.sh

--- firewall_services.sh
+++ firewall_services.sh
@@ -3,40 +3,42 @@
 # ==========================================
 # Managing Firewall Services using firewalld
 #
-# Student Name:
-# Roll Number:
+# Student Name:HISHAM
+# Roll Number:1U24IT044
+
 # ==========================================
 
 
 # Check current firewall services
-
+firewall-cmd --list-services
 
 
 
 # Allow HTTP service
-
+firewall-cmd --add-service=http
 
 
 # Allow HTTPS service
+firewall-cmd --add-service=https
+
 
 
 
 
 # Display enabled services
-
-
+firewall-cmd --list-services
 
 
 # Remove HTTP service
+firewall-cmd --remove-service=http
 
 
 
 
 # Allow SSH permanently
-
+firewall-cmd --add-service=ssh --permanent
 
 
 
 # Reload firewall configuration
-
-
+firewall-cmd --reload
